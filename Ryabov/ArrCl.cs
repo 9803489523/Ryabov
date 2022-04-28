35eae24d101ef71543c5a431f626e79c6754d1e4
user <sfrvvv@yandex.ru>
2022-04-28 19:25:44 +0300

    array class

---
+++ Ryabov/ArrCl.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryabov
+{
+    class ArrCl
+    {
+        //поле массива +геттер и сеттер
+        private int[] array
+        {
+            get; 
+            set;
+        }
+        //индексаторы
+        public int this[int index]
+        {
+            get { 
+                if(index < 0 || index >= array.Length)
+                    throw new ArgumentException();
+                return array[index]; 
+            }
+            set {
+                if (index < 0 || index >= array.Length)
+                    throw new ArgumentException();
+                array[index] = value; }
+        }
+        //конструктор с параметрами, , в качестве аргумента берем длину массива
+        public ArrCl(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException();
+            array = new int[size];
+            Console.WriteLine("Введите массив");
+            for(int i=0; i < size; i++)
+            {
+                Console.Write($"{i + 1} элемент: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+        //конструктор с параметром, в качестве аргумента берем массив
+        public ArrCl(int[] arr)
+        {
+            this.array= arr;
+        }
+        //конструктор без параметров, созлается массив размерности 0
+        public ArrCl() { array = new int[0]; }
+        //перегрузка оператора сложения двух массивов
+        public static ArrCl operator +(ArrCl arr1, ArrCl arr2)
+        {   
+            if(arr1.array.Length != arr2.array.Length)
+                throw new ArgumentException();
+            int[] arr = new int[arr1.array.Length];
+            ArrCl sum=new ArrCl(arr);
+            for(int i=0; i<arr1.array.Length; i++)
+            {
+                sum.array[i] = arr1.array[i]+ arr2.array[i];
+            }
+            return sum;
+        }
+        //перегрузка оператора разности двух массивов
+        public static ArrCl operator -(ArrCl arr1, ArrCl arr2)
+        {
+            if (arr1.array.Length != arr2.array.Length)
+                throw new ArgumentException();
+            int[] arr = new int[arr1.array.Length];
+            ArrCl sum = new ArrCl(arr);
+            for (int i = 0; i < arr1.array.Length; i++)
+            {
+                sum.array[i] = arr1.array[i] - arr2.array[i];
+            }
+            return sum;
+        }
+        //перегрузка оператора умножения на число
+        public static ArrCl operator *(ArrCl arr,int vector)
+        { 
+            for (int i = 0; i < arr.array.Length; i++)
+            {
+               arr.array[i]*= vector;
+            }
+            return arr;
+         
+        }
+        //перегрузка оператора деления на число
+        public static ArrCl operator /(ArrCl arr, int vector)
+        {
+            for (int i = 0; i < arr.array.Length; i++)
+            {
+                arr.array[i] /= vector;
+            }
+            return arr;
+        }
+
+        //вывод на печать всего массива
+        public void print()
+        {
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                Console.Write(array[i]+", ");
+            }
+            Console.Write(array[array.Length-1]+".\n");
+        }
+        //вывод на печать элемента, получаемого по индексу
+        public void printByIndex(int index)
+        {
+            if (index < 0 || index >= array.Length)
+            {
+                Console.WriteLine("Выход за границы массива");
+                return;
+            }
+            Console.WriteLine($"{index}-элемент = {array[index]}");
+        }
+    }
+}
---
+++ Ryabov/Program.cs
@@ -0,0 +1,38 @@
+﻿class Test
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            //массив ждя быстрого создания класса (без ввода с консоли)
+            int[] arr = { 1, 6, 8, 2, -9 };
+            //массив для создание класса с пустым массивом (для отслеживания дальнейших преобразований)
+            int[] empty = { 0, 0, 0, 0, 0 };
+            Ryabov.ArrCl arr1 = new(5);
+            //проверка вывода на консоль массива/элемента массива
+            arr1.print();
+            arr1.printByIndex(4);
+            arr1.printByIndex(5);
+            Ryabov.ArrCl arr3 = new(empty);
+            Ryabov.ArrCl arr2 = new Ryabov.ArrCl(arr);
+            //проверка сложения массивов
+            arr3 = arr2 + arr1;
+            arr3.print();
+            //проверка вычитания массивов
+            arr3 = arr2 - arr1;
+            arr3.print();
+            //проверка умножения массива на число
+            arr3 = arr2 * 4;
+            arr3.print();
+            //проверка деления массива на скаляр
+            arr3 = arr2 / 2;
+            arr3.print();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Введены некорректные данные! Ошибка:\n");
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
+
